563ffcb583f272a9b4f22d56b2bab9b15b3ca423
victorhugodosantos <161535416+victorhugodosantos@users.noreply.github.com>
2025-04-03 19:48:11 -0300

    tt

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let visor = document.getElementById('visor');
+let valorAtual = '';
 
+function adicionarValor(valor) {
+    valorAtual += valor;
+    visor.value = valorAtual;
+}
+
+function calcular() {
+    try {
+        let resultado = eval(valorAtual);
+        visor.value = resultado;
+        valorAtual = resultado.toString();
+    } catch (e) {
+        visor.value = 'Erro';
+        valorAtual = '';
+    }
+}
+
+function limpar() {
+    valorAtual = '';
+    visor.value = '';
+}
+
+function backspace() {
+    valorAtual = valorAtual.slice(0, -1);
+    visor.value = valorAtual;
+}
